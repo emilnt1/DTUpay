cc064faf15e623b347c5bde84314a76538deee9c
Emil Trangeled <emilbfh@hotmail.com>
2023-01-19 21:50:23 +0100

    mabyw orks on jenkins

--- CustomerService/src/main/java/org/example/init.java
+++ CustomerService/src/main/java/org/example/init.java
@@ -17,7 +17,7 @@ public class init {
     public void startUp() throws Exception{
         System.out.println("startup");
 
-        var mq = new RabbitMqQueue("localhost");
+        var mq = new RabbitMqQueue("10.5.0.5");
         new CustomerService(mq);
 
     }
