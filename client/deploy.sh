d08cd4e59b4d43b7444c0af4b1bee99573375e94
Emil Trangeled <emilbfh@hotmail.com>
2023-01-20 09:27:59 +0100

    Mabye works on jenkins

--- client/deploy.sh
+++ client/deploy.sh
@@ -3,5 +3,7 @@ set -e
 docker image prune -f
 docker-compose up -d rabbitMq
 sleep 10
-docker-compose up -d dtupay tokenservice customerservice
+docker-compose up -d tokenservice customerservice
+
+
 
