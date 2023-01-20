5129161c35272be357709de4ed18892e92cdddb9
Fiedler12 <o.lyngholm@gmail.com>
2023-01-20 18:35:12 +0100

    Nyt til Jenkins

--- client/deploy.sh
+++ client/deploy.sh
@@ -3,5 +3,7 @@ set -e
 docker image prune -f
 docker-compose up -d rabbitMq
 sleep 10
+docker-compose up -d dtupay_backend
+sleep 15
 docker-compose up -d tokenservice customerservice merchantservice
 
